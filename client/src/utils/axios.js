181b195591244ac97ad8055f13eacb6800b2cf3d
Jesse <hambergjesse@gmail.com>
2025-01-06 13:06:28 +0200

    axios url

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const uri = import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: uri,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
