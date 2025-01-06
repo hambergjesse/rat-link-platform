faf78a174fc26539da4bd52b9d8a51c4eb72210d
Jesse <hambergjesse@gmail.com>
2025-01-06 21:00:49 +0200

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": "http://localhost:3001",
-      "/auth": "http://localhost:3001",
+      "/api": process.env.VITE_API_URL || "http://localhost:3001",
+      "/auth": process.env.VITE_API_URL || "http://localhost:3001",
     },
   },
 });
