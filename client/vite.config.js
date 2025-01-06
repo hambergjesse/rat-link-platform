4938efc8f59da7f95fb9ffa7bb61376b67015743
Jesse <hambergjesse@gmail.com>
2025-01-06 21:07:53 +0200

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: process.env.NODE_ENV === "development" ? 5173 : undefined,
     proxy: {
       "/api": process.env.VITE_API_URL || "http://localhost:3001",
       "/auth": process.env.VITE_API_URL || "http://localhost:3001",
